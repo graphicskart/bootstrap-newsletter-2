36c326bb0f80657382062946920963e7b66bd886
graphicskart <graphicskart2018@gmail.com>
2019-03-31 21:47:49 +0530

    Updates

---
+++ precache-manifest.49505621555f487359bbe7444842a069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7a61ca358bd692f660ce10687b195ae",
+    "url": "/bootstrap-newsletter-2/static/media/img.d7a61ca3.png"
+  },
+  {
+    "revision": "4c465a84fe1bdb81a90f",
+    "url": "/bootstrap-newsletter-2/static/js/runtime~main.0e9345f1.js"
+  },
+  {
+    "revision": "9a57a4dbf77e8c72f640",
+    "url": "/bootstrap-newsletter-2/static/js/main.48da9b29.chunk.js"
+  },
+  {
+    "revision": "89ea03f4fe53eb71d386",
+    "url": "/bootstrap-newsletter-2/static/js/2.93a59ba5.chunk.js"
+  },
+  {
+    "revision": "9a57a4dbf77e8c72f640",
+    "url": "/bootstrap-newsletter-2/static/css/main.e12360be.chunk.css"
+  },
+  {
+    "revision": "c36ebd6865a701d4515df87bdf810b18",
+    "url": "/bootstrap-newsletter-2/index.html"
+  }
+];
